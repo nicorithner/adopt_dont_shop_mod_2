806e3e12170a89064b1d4a3a591881459f19bba7
Nico Rithner <nicorithner@gmail.com>
2020-08-18 04:24:57 -0600

    Debug form and create method

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,21 +1,24 @@
 class SheltersController < ApplicationController
   def index
-    @shelters = ['shelter 1', 'shelter 2', 'shelter 3']
+    @shelters = Shelter.all
   end
 
   def new
   end
 
   def create
+    binding.pry
     shelter = Shelter.new({
       name: params[:shelter][:name],
       address: params[:shelter][:address],
       city: params[:shelter][:city],
       state: params[:shelter][:state],
-      zip: params[:shelter][:zip],
+      zip: params[:shelter][:zip]
       })
+    binding.pry
 
     shelter.save
+    binding.pry
 
     redirect_to '/shelters'
   end
