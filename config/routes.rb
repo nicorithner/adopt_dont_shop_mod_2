cd043e737545e064389c41d6d8c01e4c75077172
Nico Rithner <nicorithner@gmail.com>
2020-08-18 05:46:58 -0600

    Add route to shelters#show

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   post '/shelters', to: 'shelters#create'
+  get '/shelters/:id', to: 'shelters#show'
 end
