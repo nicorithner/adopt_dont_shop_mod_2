86c856d7f8fb6c4ed646c5ccb536ab7df44fb07c
Nico Rithner <nicorithner@gmail.com>
2020-08-17 20:17:08 -0600

    Create form, create shelter and database

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -2,4 +2,21 @@ class SheltersController < ApplicationController
   def index
     @shelters = ['shelter 1', 'shelter 2', 'shelter 3']
   end
+
+  def new
+  end
+
+  def create
+    shelter = Shelter.new({
+      name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip],
+      })
+
+    shelter.save
+
+    redirect_to '/shelters'
+  end
 end
