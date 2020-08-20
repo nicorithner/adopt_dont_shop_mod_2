fbf545642ed866c960993cdb8e7fd0420982350a
Nico Rithner <nicorithner@gmail.com>
2020-08-20 12:18:04 -0600

    Complete test the visitor can see pets information when visiting '/pets'

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -3,25 +3,23 @@ require 'rails_helper'
 RSpec.describe "pets index page" do
 
   before :each do
-    @shelter_1 = Shelter.create(name: "Shelter 1", address: "123 st", city: "Denver", state: "CO", zip: 80202)
-    @shelter_2 = Shelter.create(name: "Shelter 2", address: "123 road", city: "Denver", state: "CO", zip: 80201)
+    @shelter_1 = Shelter.create!(name: "Shelter 1", address: "123 st", city: "Denver", state: "CO", zip: 80202)
+    @shelter_2 = Shelter.create!(name: "Shelter 2", address: "123 road", city: "Denver", state: "CO", zip: 80201)
 
-    @pet_1 = Pet.create(image: "brown_puppy.jpg", name: "Puppy1", age: 1, sex: "Male", shelter_id: @shelter_1.id)
-    @pet_2 = Pet.create(image: "english_bulldog.jpg", name: "Puppy2", age: 1, sex: "Female", shelter_id: @shelter_1.id)
-    @pet_3 = Pet.create(image: "golden.jpg", name: "Puppy3", age: 2, sex: "Female", shelter_id: @shelter_2.id)
-    @pet_4 = Pet.create(image: "cat.jpg", name: "Kitten1", age: 2, sex: "Male", shelter_id: @shelter_2.id)
+    @pet_1 = Pet.create!(image: "brown_puppy.jpg", name: "Puppy1", age: 1, sex: "Male", shelter_id: @shelter_1.id)
+    @pet_2 = Pet.create!(image: "english_bulldog.jpg", name: "Puppy2", age: 1, sex: "Female", shelter_id: @shelter_1.id)
+    @pet_3 = Pet.create!(image: "golden.jpg", name: "Puppy3", age: 2, sex: "Female", shelter_id: @shelter_2.id)
+    @pet_4 = Pet.create!(image: "cat.jpg", name: "Kitten1", age: 2, sex: "Male", shelter_id: @shelter_2.id)
   end
 
   it "Can see each pet in the system info" do
     visit "/pets"
-# save_and_open_page
-    expect(page).to have_xpath("link[@href='brown_puppy.jpg']")
-    # expect(page).to have_css('img', text: "brown_puppy.jpg")
+
+    expect(page).to have_xpath("//img['brown_puppy.jpg']")
     expect(page).to have_content(@pet_1.name)
     expect(page).to have_content(@pet_1.age)
     expect(page).to have_content(@pet_1.sex)
     expect(page).to have_content(@pet_1.shelter.name)
-    
   end
 
 end
