fea1b02f0c149a29535ffc0e42eae1f298bf27f6
Nico Rithner <nicorithner@gmail.com>
2020-08-20 06:39:19 -0600

    Run rails db:migrate to complete migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_012732) do
+ActiveRecord::Schema.define(version: 2020_08_20_123654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_08_20_012732) do
     t.string "sex"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "shelter_id"
+    t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
   create_table "shelters", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema.define(version: 2020_08_20_012732) do
     t.integer "zip"
   end
 
+  add_foreign_key "pets", "shelters"
 end
