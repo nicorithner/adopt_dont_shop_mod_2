dc50d9304c1e82326275c10a7fe234ce3eab59d6
Nico Rithner <nicorithner@gmail.com>
2020-08-20 12:58:16 -0600

    Upgrade routes and created a 'pets.html.erb' for sheleters

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
-
-  get '/pets', to: 'pets#index'
   
+  get '/pets', to: 'pets#index'
+  get '/shelters/:shelter_id/pets', to: 'shelters#pets_index'
 end
