72c80e5032f0834211fafd78dc346cfd8ce50cfb
Travis McKinstry <mckinstrytg@gmail.com>
2020-08-24 18:59:52 -0600

    Model test for reviews pass

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_000549) do
+ActiveRecord::Schema.define(version: 2020_08_25_005718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 2020_08_21_000549) do
     t.string "status"
     t.text "description"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.integer "rating"
+    t.string "content"
   end
 
   create_table "shelters", force: :cascade do |t|
