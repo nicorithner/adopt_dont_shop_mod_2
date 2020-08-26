63ee80bd92ba0307b555965daaa6468956090937
Nico Rithner <nicorithner@gmail.com>
2020-08-26 13:16:08 -0600

    Updated features test

--- spec/features/favorites/index_spec.rb
+++ spec/features/favorites/index_spec.rb
@@ -14,7 +14,6 @@ RSpec.describe "pets index page" do
 
   it " There is a '/favorites' index page, and Favorite pets are listed there" do
     visit "/favorites"
-    save_and_open_page
     expect(current_path).to eq("/favorites")
     expect(page).to have_content("Puppy2")
     expect(page).to have_content("Puppy3")
@@ -27,8 +26,12 @@ RSpec.describe "pets index page" do
   end
 
   it "Displays pet's image and pet's name is a link to pet's show page" do
+    visit "/pets/#{@pet_1.id}"
+    click_on "Favorite"
+    @pet_1.reload
     visit "/pets"
-
+    save_and_open_page
+    expect(page).to have_content("Favorite 3")
     expect(page).to have_selector(:link_or_button, 'Favorite')
     expect(page).to have_xpath("//img['brown_puppy.jpg']")
   end
