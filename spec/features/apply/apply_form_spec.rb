628dfef95bc363172dc48ee79659b28242e2a855
Travis McKinstry <mckinstrytg@gmail.com>
2020-08-26 18:44:02 -0600

    Get first part of adopt test done

--- spec/features/apply/apply_form_spec.rb
+++ spec/features/apply/apply_form_spec.rb
@@ -7,9 +7,6 @@ RSpec.describe "Apply for pet" do
     @shelter_2 = Shelter.create!(name: "Shelter 2", address: "123 road", city: "Denver", state: "CO", zip: 80201)
 
     @pet_1 = Pet.create!(image: "brown_puppy.jpg", name: "Puppy1", age: 1, sex: "Male", shelter_id: @shelter_1.id, favorite: "false")
-    @pet_2 = Pet.create!(image: "english_bulldog.jpg", name: "Puppy2", age: 1, sex: "Female", shelter_id: @shelter_1.id, favorite: "true")
-    @pet_3 = Pet.create!(image: "golden.jpg", name: "Puppy3", age: 2, sex: "Female", shelter_id: @shelter_2.id, favorite: "true")
-    @pet_4 = Pet.create!(image: "cat.jpg", name: "Kitten1", age: 2, sex: "Male", shelter_id: @shelter_2.id, favorite: "false")
   end
 
   it "When I have added pets to my favorites list" do
@@ -32,6 +29,10 @@ RSpec.describe "Apply for pet" do
 
       expect(current_path).to eq("/pets/#{@pet_1.id}/adopt")
 
+
+      within("##{@pet_1.name}") do
+        choose('adopt')
+      end
 
   end
 
