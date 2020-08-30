2df3a1f228c3bc56d8a5a6f454405e6e99609589
Nico Rithner <nicorithner@gmail.com>
2020-08-29 20:16:07 -0600

    Add action add_favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,4 +3,23 @@ class FavoritesController < ApplicationController
     @favorites = FavoritePet.new(session[:favorite])
     @favorites.favorites
   end
+
+  def add_favorite
+    @pet = Pet.find(params[:id])
+    @favorites = FavoritePet.new(session[:favorite])
+    @favorites.add_pet(@pet)
+    redirect_to("/pets/#{@pet.id}")
+
+    if @favorites.favorited?(@pet)
+      flash[:notice] = "Pet saved to favorites"
+    else
+      flash[:notice] = "Pet Removed from favorites"
+    end
+  end
+
+  #===Needs to be refactor
+  # def remove_all_favorites
+  # @pets = Pet.all
+  # @pets.total_favorites.map {|pet| pet.update(favorite: !pet.favorite)}
+  # end
 end
