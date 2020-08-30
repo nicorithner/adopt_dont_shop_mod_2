eb6f3fe49d52aeb56e2fc75c403d0e4d8a639f71
Nico Rithner <nicorithner@gmail.com>
2020-08-29 20:08:56 -0600

    Remove obsolete favorites methods

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,6 +5,7 @@ class PetsController < ApplicationController
 
   def show
     @pet = Pet.find(params[:id])
+    @favorites = FavoritePet.new(session[:favorite])
   end
 
   def new
@@ -32,25 +33,8 @@ class PetsController < ApplicationController
     redirect_to "/pets"
   end
 
-  def toogle_favorite
-    @pet = Pet.find(params[:id])
-    @pet.update(favorite: !@pet.favorite)
-    redirect_to("/pets/#{@pet.id}")
-    if @pet.favorite
-      flash[:notice] = "Pet saved to favorites"
-    else
-      flash[:notice] = "Pet Removed from favorites"
-    end
-    # render :new
-  end
-
   def adopt
     @favorites = Pet.where(favorite: "true")
-  end
-  
-  def remove_all_favorites
-    @pets = Pet.all
-    @pets.total_favorites.map {|pet| pet.update(favorite: !pet.favorite)}
   end
 
   private
