b2e55fbfbd1611b35e4b02634f14e3a46d0406c1
Nico Rithner <nicorithner@gmail.com>
2020-08-30 15:00:09 -0600

    Upgrade features test to reflect changes from database to session use

--- spec/features/favorites/refactor_favorites.rb
+++ spec/features/favorites/refactor_favorites.rb
@@ -5,8 +5,8 @@ RSpec.describe "pets index page" do
   before :each do
     @shelter_1 = Shelter.create!(name: "Shelter 1", address: "123 st", city: "Denver", state: "CO", zip: 80202)
     @shelter_2 = Shelter.create!(name: "Shelter 2", address: "123 road", city: "Denver", state: "CO", zip: 80201)
-
-    @pet_1 = Pet.create!(image: "brown_puppy.jpg", name: "Puppy1", age: 1, sex: "Male", shelter_id: @shelter_1.id)
+  
+    @pet_1 = @shelter_1.pets.create!(image: "brown_puppy.jpg", name: "Puppy1", age: 1, sex: "Male")
     @pet_2 = Pet.create!(image: "english_bulldog.jpg", name: "Puppy2", age: 1, sex: "Female", shelter_id: @shelter_1.id)
     @pet_3 = Pet.create!(image: "golden.jpg", name: "Puppy3", age: 2, sex: "Female", shelter_id: @shelter_2.id)
     @pet_4 = Pet.create!(image: "cat.jpg", name: "Kitten1", age: 2, sex: "Male", shelter_id: @shelter_2.id)
@@ -49,12 +49,12 @@ RSpec.describe "pets index page" do
       expect(page).to have_content("#{@pet_1.name}")
     end 
 
-    # it "Favorite count in nav bar is updated after clicking fav link" do
-    #   visit "/pets/#{@pet_1.id}"
-    #   click_on "Favorite"
-    #   @pet_1.reload
-    #   visit "/pets"
-    #   expect(page).to have_content("Favorite 3")
-    # end
+    it "Favorite count in nav bar is updated after clicking fav link" do
+      visit "/pets/#{@pet_1.id}"
+      click_on "Favorite"
+      @pet_1.reload
+      visit "/pets"
+      expect(page).to have_content("Favorite 1")
+    end
   end
 end
