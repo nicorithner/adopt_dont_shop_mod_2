d9c7434dc6636e5101a1e77d62123fd8b3359c63
Nico Rithner <nicorithner@gmail.com>
2020-08-30 14:48:41 -0600

    Upgrade favorites_controller action add_favorite and create remove_favorite action for 'Favorite' and 'Remove Favorite' links

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,25 +1,24 @@
 class FavoritesController < ApplicationController
+
   def index
-    @favorites = FavoritePet.new(session[:favorite])
-    @favorites.favorites
+    @favorites = session_favorites
   end
 
   def add_favorite
     @pet = Pet.find(params[:id])
-    @favorites = FavoritePet.new(session[:favorite])
-    @favorites.add_pet(@pet)
+    session_favorites << @pet
     redirect_to("/pets/#{@pet.id}")
 
-    if @favorites.favorited?(@pet)
+    if session_favorites.include?(@pet)
       flash[:notice] = "Pet saved to favorites"
     else
       flash[:notice] = "Pet Removed from favorites"
     end
   end
 
-  #===Needs to be refactor
-  # def remove_all_favorites
-  # @pets = Pet.all
-  # @pets.total_favorites.map {|pet| pet.update(favorite: !pet.favorite)}
-  # end
+  def remove_favorite
+    pet = Pet.find(params[:id])
+    session_favorites.delete(pet)
+  end
+
 end
