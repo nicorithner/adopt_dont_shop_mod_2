3bf30cb1df58bdeb0d77716c9ee0f1323244c96e
Nico Rithner <nicorithner@gmail.com>
2020-08-29 18:52:41 -0600

    Remove obsolete class methods for favorites

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,14 +1,5 @@
 class Pet < ApplicationRecord
   validates_presence_of :image, :name, :age, :sex, :shelter, favorite: false
 
-  belongs_to :shelter
-
-  def self.favorites
-    Pet.where(favorite: "true").count
-  end
-  
-  def self.total_favorites
-    Pet.where(favorite: "true")
-  end
-    
+  belongs_to :shelter    
 end
