6b0d6c7c5a947c0dcdfba4565d59dc72a507aec6
Travis McKinstry <mckinstrytg@gmail.com>
2020-08-31 17:52:01 -0600

    Pass user story 23 test

--- spec/features/application/approve_spec.rb
+++ spec/features/application/approve_spec.rb
@@ -42,5 +42,38 @@ RSpec.describe "Approve an application" do
         end
       end
     end
+    describe "When an application is made for more than one pet" do
+      describe "When I visit that applications show page" do
+        it "I'm able to approve the application for any number of pets" do
+          shelter_1 = Shelter.create(name: "Shelter 1", address: "123 st", city: "Denver", state: "CO", zip: 80202)
+          shelter_2 = Shelter.create(name: "Shelter 2", address: "123 road", city: "Denver", state: "CO", zip: 80201)
+          pet_1 = Pet.create(image: "brown_puppy.jpg", name: "Puppy1", age: 1, sex: "Male", shelter_id: shelter_1.id)
+          pet_2 = Pet.create(image: "english_bulldog.jpg", name: "Puppy2", age: 1, sex: "Female", shelter_id: shelter_1.id)
+          pet_3 = Pet.create(image: "golden.jpg", name: "Puppy3", age: 2, sex: "Female", shelter_id: shelter_2.id)
+          pet_4 = Pet.create(image: "cat.jpg", name: "Kitten1", age: 2, sex: "Male", shelter_id: shelter_2.id)
+
+          app_1 = App.create!(name: "Bob", city: "Denver", state: "CO", zip: 87665, phone_number: "3039997865", address: "5576 South Yup Street", description: "yup")
+          app_2 = App.create!(name: "Tim", city: "Lakewood", state: "UT", zip: 344900, phone_number: "7889007654", address: "5576 North La Road", description: "This is a description")
+
+          ApplicationPet.create!(app: app_1, pet: pet_1)
+          ApplicationPet.create!(app: app_1, pet: pet_3)
+          ApplicationPet.create!(app: app_2, pet: pet_2)
+
+          visit "/applications/#{app_1.id}"
+          expect(page).to have_content("#{pet_1.name}")
+
+          within("#application-#{app_1.id}") do
+            click_on("Approve application for #{pet_1.name}")
+          end
+
+          visit "/applications/#{app_1.id}"
+          expect(page).to have_content("#{pet_3.name}")
+
+          within("#application-#{app_1.id}") do
+            click_on("Approve application for #{pet_3.name}")
+          end
+        end
+      end
+    end
   end
 end
