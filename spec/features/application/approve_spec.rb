414edf0ae8cb79a0dda9739d6ecd210ed4f666c8
Travis McKinstry <mckinstrytg@gmail.com>
2020-08-31 18:41:25 -0600

    Pass user story 24

--- spec/features/application/approve_spec.rb
+++ spec/features/application/approve_spec.rb
@@ -75,5 +75,31 @@ RSpec.describe "Approve an application" do
         end
       end
     end
+    describe "When a pet has more than one application made for them" do
+      describe "And one application has already been approved for them" do
+        it "I can not approve any other applications for that pet but all other applications still remain on file (they can be seen on the pets application index page)" do
+          shelter_1 = Shelter.create(name: "Shelter 1", address: "123 st", city: "Denver", state: "CO", zip: 80202)
+          shelter_2 = Shelter.create(name: "Shelter 2", address: "123 road", city: "Denver", state: "CO", zip: 80201)
+          pet_1 = Pet.create(image: "brown_puppy.jpg", name: "Puppy1", age: 1, sex: "Male", shelter_id: shelter_1.id, application_pending: true)
+          pet_2 = Pet.create(image: "english_bulldog.jpg", name: "Puppy2", age: 1, sex: "Female", shelter_id: shelter_1.id)
+          pet_3 = Pet.create(image: "golden.jpg", name: "Puppy3", age: 2, sex: "Female", shelter_id: shelter_2.id)
+          pet_4 = Pet.create(image: "cat.jpg", name: "Kitten1", age: 2, sex: "Male", shelter_id: shelter_2.id)
+
+          app_1 = App.create!(name: "Bob", city: "Denver", state: "CO", zip: 87665, phone_number: "3039997865", address: "5576 South Yup Street", description: "yup")
+          app_2 = App.create!(name: "Tim", city: "Lakewood", state: "UT", zip: 344900, phone_number: "7889007654", address: "5576 North La Road", description: "This is a description")
+
+          ApplicationPet.create!(app: app_1, pet: pet_1)
+          ApplicationPet.create!(app: app_1, pet: pet_3)
+          ApplicationPet.create!(app: app_2, pet: pet_1)
+
+          visit "/applications/#{app_1.id}"
+          expect(page).to have_content("#{pet_1.name}")
+
+          visit "/applications/#{app_2.id}"
+
+          expect(page).to have_content("#{pet_1.name} already has an application pending")
+        end
+      end
+    end
   end
 end
