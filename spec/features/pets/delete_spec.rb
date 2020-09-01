aba5e25eec0da6b1bc3279234717de71863e9689
Travis McKinstry <mckinstrytg@gmail.com>
2020-09-01 17:53:22 -0600

    Pass user story 31 test

--- spec/features/pets/delete_spec.rb
+++ spec/features/pets/delete_spec.rb
@@ -18,16 +18,11 @@ RSpec.describe "Pet Delete" do
 
   it "can't delete a pet with application pending" do
     shelter_1 = Shelter.create!(name: "Shelter 1", address: "123 lane", city: "Denver", state: "CO", zip: 80202)
-    pet_1 = Pet.create!(image: "frog.jpg", name: "Toby", age: 1, sex: "Male", shelter: shelter_1)
+    pet_1 = Pet.create!(image: "frog.jpg", name: "Toby", age: 1, sex: "Male", shelter: shelter_1, application_pending: true)
 
     visit "/pets/#{pet_1.id}"
-    expect(page).to have_selector(:link_or_button, 'Delete Pet')
+    expect(page).to_not have_selector(:link_or_button, 'Delete Pet')
 
-    expect(page).to have_content("#{pet_1.name}")
-    click_on "Delete Pet"
-
-    expect(current_path).to eq("/pets")
-    expect(page).to_not have_content("#{pet_1.name}")
   end
 
 end
